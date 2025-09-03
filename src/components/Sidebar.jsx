9aecd74be0ec2e783a899853950da641d3045580
oxfrz <wassim.hamrit@etu.univ-amu.fr>
2025-09-03 15:54:08 +0200

    fix: update Sidebar links for GitHub Pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -187,7 +187,7 @@ function Sidebar(props) {
         {status}
       </motion.p>
 
-      <a href="./index.html">
+      <a href={`${import.meta.env.BASE_URL}index.html`}>
         <motion.div
           className=" dark:after:bg-slate-700 dark:bg-slate-700 hover:after:dark:bg-slate-800 bg-zinc-400  after:hover:bg-zinc-500  after:transition-all after:duration-300  shadow-lg dark:shadow-slate-500/20 pr-3 pl-3 h-12 rounded-lg flex justify-center items-center mt-7 mb-7 cursor-pointer hover:h-13 transition-all duration-300 relative z-10 hover:before:absolute hover:before:inset-1 hover:before:bg-gradient-to-br  hover:before:from-cyan-400/70 hover:before:to-fuchsia-500/70 hover:before:rounded-lg hover:before:z-0 before:blur-lg before:transition-all before:duration-300 after:absolute after:inset-0 after:z-10 after:rounded-lg after:bg-zinc-400 before:animate-spin"
           variants={elementsVariants}
@@ -202,7 +202,7 @@ function Sidebar(props) {
         </motion.div>
       </a>
 
-      <a href="../../links.html">
+      <a href={`${import.meta.env.BASE_URL}links.html`}>
         <motion.div
           className="  dark:after:bg-slate-700 dark:bg-slate-700 hover:after:dark:bg-slate-800 bg-zinc-400  after:hover:bg-zinc-500 after:transition-all after:duration-300 shadow-lg dark:shadow-slate-500/20 pr-3 pl-3 h-12 rounded-lg flex justify-center items-center mb-7 cursor-pointer hover:h-13 transition-all duration-300 relative z-10 hover:before:absolute hover:before:inset-1 hover:before:bg-gradient-to-br  hover:before:from-cyan-400/70 hover:before:to-fuchsia-500/70 hover:before:rounded-lg hover:before:z-0 before:blur-lg before:transition-all before:duration-300 after:absolute after:inset-0 after:z-10 after:rounded-lg after:bg-zinc-400 before:animate-spin"
           variants={elementsVariants}
@@ -286,7 +286,7 @@ function Sidebar(props) {
         </motion.div>
       )}
 
-      <a href="../../drawings.html">
+      <a href={`${import.meta.env.BASE_URL}drawings.html`}>
         <motion.div
           className="bg-zinc-400 shadow-lg dark:shadow-slate-500/20 pr-3 pl-3 h-12 rounded-lg flex justify-center items-center mb-7 cursor-pointer hover:h-13 transition-all duration-300 relative z-10 hover:before:absolute hover:before:inset-1 hover:before:bg-gradient-to-br  hover:before:from-cyan-400/70 hover:before:to-fuchsia-500/70 hover:before:rounded-lg hover:before:z-0 before:blur-lg before:transition-all before:duration-300 after:absolute after:inset-0 after:z-10 after:rounded-lg after:bg-zinc-400 after:hover:bg-zinc-500 after:transition-all after:duration-300 dark:after:bg-slate-700 dark:bg-slate-700 hover:after:dark:bg-slate-800 before:animate-spin"
           variants={elementsVariants}
